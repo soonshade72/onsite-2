03153c2a9df823044bd64a6530a409596128a283
Yashraj Singh <yashrajsinghrajawat07@gmail.com>
2026-07-30 20:15:13 +0530

    Add files via upload

---
+++ server 4/server4.py
@@ -0,0 +1,18 @@
+from flask import Flask,jsonify,request
+import socket
+app=Flask(__name__)
+total=0
+unique=set()
+identity= socket.gethostname()
+@app.route("/")
+def home():
+    global total,unique 
+    ip=request.remote_addr
+    unique.add(ip)
+    return jsonify({
+        "server_identity":identity,
+        "total_requests": total,
+        "unique_users":len(unique)
+    })
+if _name_=="main":
+    app.run(host="0.0.0.0",port=5004)
